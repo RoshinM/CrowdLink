cdc30abd87c7a760638849bd7f9cac94d69d51f6
RoshinM <roshinmaharjan7@gmail.com>
2024-11-22 20:08:53 +0545

    login page basic UI layout

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -11,12 +11,31 @@ class Loginpage extends StatefulWidget {
 class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-
-        ],
+      body: Container(
+        color: Colors.black87,
+        child: Column(
+          children: [
+            Image.asset("assets/images/crowdLinkLogo.png"),
+            Expanded(
+              child: Container(
+                width: size.width,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text("Your content here"),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
