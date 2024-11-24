4fcdd49e7d4216d14013b9d1d743127e121ccd2c
RoshinM <roshinmaharjan7@gmail.com>
2024-11-24 13:46:45 +0545

    basic structure for Home page with top Nav bar

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +9,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  topNavBar(){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 22),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25),
+          bottomRight: Radius.circular(25),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'CrowdLink',
+            style: GoogleFonts.outfit(fontSize: 32,
+                fontWeight: FontWeight.bold),
+          ),
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.notifications_outlined),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.mail_outline),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  homePageBody(Size size){
+    return Expanded(
+        child: Container(
+          width: size.width,
+          padding: EdgeInsets.only(left: 18,top: 24,right: 18),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25),
+              topRight: Radius.circular(25),
+            ),
+          ),
+          child: ListView(//Vertical Scroll Listview
+          children: [
+              Text("Make the list view here"),
+            ],
+          ),
+        ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("homepage"),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          //Need to make space for system top bar
+          topNavBar(),
+          SizedBox(height: 1,),
+          homePageBody(size),
+        ],
+      ),
     );
   }
 }
